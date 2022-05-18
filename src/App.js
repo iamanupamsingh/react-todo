387e17c85901c9316888445ba03e303a80723e55
iamanupamsingh <anupamsinghaj@gmail.com>
2022-05-18 19:26:30 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TodoForm from './components/todoform';
+import TodoList from './components/todolist'
+
+function App() {
+  return (
+    <div className="todo-app">
+     <TodoList/>
+      
+    </div>
+  );
+}
+
+export default App;
